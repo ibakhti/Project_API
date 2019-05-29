ed97a3596c066bdf9b26527d3a68bcd11818ac89
ibakhti <ilhambakhti06.ib@gmail.com>
2019-05-29 20:33:23 +0700

    add orderRouter for checkout component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const userRouter = require('./routers/userRouter');
-const productRouter = require('./routers/productRouter');
+const userRouter = require("./routers/userRouter");
+const productRouter = require("./routers/productRouter");
+const orderRouter = require("./routers/orderRouter");
 
 app = express();
 port = 8080;
@@ -10,11 +11,9 @@ port = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(userRouter);
-app.use(productRouter)
-
-
-
+app.use(productRouter);
+app.use(orderRouter);
 
 app.listen(port, () => {
-    console.log('API Running at Port: ' + port)
-})
+  console.log("API Running at Port: " + port);
+});
---
+++ src/routers/orderRouter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const conn = require('../connection/connection');
+
+//----------GET ITMES FOR CHECKOUT-----------//
+router.get('/checkout', (req, res) => {
+    const sql = `SELECT * FROM checkoutItemView WHERE userId = ${req.query.userId}`;
+    const sql2 = `SELECT SUM(total) subTotal FROM checkoutItemView GROUP BY userId HAVING userId = ${req.query.userId}`
+
+    conn.query(sql, (err, result) => {
+        if(err) return res.status(400).send(err.sqlMessage)
+
+        conn.query(sql2, (err2, result2) => {
+            if(err2) return res.status(400).send("err2: " + err2.sqlMessage)
+
+            res.send([result, result2])
+        })
+    })
+})
+
+module.exports = router
