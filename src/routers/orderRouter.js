ab3ac7bec41a4e2239167e6490e57034ceb3c1e3
ibakhti <ilhambakhti06.ib@gmail.com>
2019-05-31 21:35:10 +0700

    add shipping API

--- src/routers/orderRouter.js
+++ src/routers/orderRouter.js
@@ -1,20 +1,47 @@
-const router = require('express').Router();
-const conn = require('../connection/connection');
+const router = require("express").Router();
+const conn = require("../connection/connection");
 
 //----------GET ITMES FOR CHECKOUT-----------//
-router.get('/checkout', (req, res) => {
-    const sql = `SELECT * FROM checkoutItemView WHERE userId = ${req.query.userId}`;
-    const sql2 = `SELECT SUM(total) subTotal FROM checkoutItemView GROUP BY userId HAVING userId = ${req.query.userId}`
+router.get("/checkout", (req, res) => {
+  const sql = `SELECT * FROM checkoutItemView WHERE userId = ${
+    req.query.userId
+  }`;
+  const sql2 = `SELECT SUM(total) subTotal FROM checkoutItemView GROUP BY userId HAVING userId = ${
+    req.query.userId
+  }`;
+
+  conn.query(sql, (err, result) => {
+    if (err) return res.status(400).send(err.sqlMessage);
+
+    conn.query(sql2, (err2, result2) => {
+      if (err2) return res.status(400).send("err2: " + err2.sqlMessage);
+
+      res.send([result, result2]);
+    });
+  });
+});
+
+//--------------GET ALL SHIPPING METHOD------------//
+router.get('/shipper', (req, res) => {
+  const sql = `SELECT * FROM shippers`
+
+  conn.query(sql, (err, result) => {
+    if(err) return res.status(400).send(err.sqlMessage);
+
+    res.status(200).send(result);
+  });
+});
+
+router.get("/shipping", (req, res) => {
+    const sql = `SELECT * FROM shippingView`;
 
     conn.query(sql, (err, result) => {
-        if(err) return res.status(400).send(err.sqlMessage)
+        if(err) return res.status(400).send(err.sqlMessage);
 
-        conn.query(sql2, (err2, result2) => {
-            if(err2) return res.status(400).send("err2: " + err2.sqlMessage)
+        res.send(result);
+    });
+});
 
-            res.send([result, result2])
-        })
-    })
-})
 
-module.exports = router
+
+module.exports = router;
